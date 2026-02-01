a46cfaeeac235e2e2af538512c11c4fe5778d6fc
00aras <155016462+00aras@users.noreply.github.com>
2026-02-01 23:48:25 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace KurWıdget
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(66, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Altın :  -";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.Location = new Point(66, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Dolar :  -";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(66, 81);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Euro :  -";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 15000;
+            timer1.Tick += timer1_TickAsync;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(280, 120);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Opacity = 0.85D;
+            StartPosition = FormStartPosition.Manual;
+            Text = "Form1";
+            TopMost = true;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,76 @@
+namespace KurWıdget
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Location = new Point(
+        Screen.PrimaryScreen.WorkingArea.Width - this.Width - 10,
+        Screen.PrimaryScreen.WorkingArea.Height - this.Height - 10);
+
+            label1.Text = "Altın :" ; 
+            label2.Text = "Dolar :" ;
+            label3.Text = "Euro  :" ;
+            timer1.Start();
+            
+
+
+        }
+
+        private async void  timer1_TickAsync(object sender, EventArgs e)
+        {
+            await DovizGuncelle();
+
+        }
+
+       
+            
+
+            private double sonDolar = 0;
+        private double sonEuro = 0;
+
+        private async Task DovizGuncelle()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = "https://api.exchangerate.host/latest?base=TRY&symbols=USD,EUR";
+                    string response = await client.GetStringAsync(url);
+
+                    JObject data = JObject.Parse(response);
+                    double dolar = (double)data["rates"]["USD"];
+                    double euro = (double)data["rates"]["EUR"];
+
+                    // Artış / azalış rengi
+                    label2.ForeColor = dolar >= sonDolar ? Color.Green : Color.Red;
+                    label3.ForeColor = euro >= sonEuro ? Color.Green : Color.Red;
+
+                    // Label güncelle
+                    label2.Text = "Dolar : " + dolar.ToString("0.##");
+                    label3.Text = "Euro : " + euro.ToString("0.##");
+
+                    // Son değerleri kaydet
+                    sonDolar = dolar;
+                    sonEuro = euro;
+                }
+            }
+            catch
+            {
+                label2.Text = "Dolar : Bağlantı yok";
+                label3.Text = "Euro : Bağlantı yok";
+                label2.ForeColor = label3.ForeColor = Color.White;
+            }
+        }
+
+    }
+
+
+
+}
+
